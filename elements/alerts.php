3dfd2aa3d1549fa5a5c25992a36fc38b177e7892
Arttu Ylikotila <arttu.ylikotila@tuni.fi>
2026-09-04 14:05:22 +0300

    Minor changes to HTML strucutre

--- elements/alerts.php
+++ elements/alerts.php
@@ -17,7 +17,9 @@
 if (!empty($errorMessage)) {
 ?>
 <div class="container content-card">
-        <div class="alert alert-danger"><?php echo htmlspecialchars($errorMessage, ENT_QUOTES, 'UTF-8') ?></div>
+    <div class="alert alert-danger">
+        <?php echo htmlspecialchars($errorMessage, ENT_QUOTES, 'UTF-8') ?>
+    </div>
 </div>
 
 <?php
@@ -26,7 +28,9 @@ if (!empty($errorMessage)) {
 if (!empty($infoMessage)) {
 ?>
 <div class="container content-card">
-        <div class="alert alert-info"><?php echo htmlspecialchars($infoMessage, ENT_QUOTES, 'UTF-8') ?></div>
+    <div class="alert alert-info">
+        <?php echo htmlspecialchars($infoMessage, ENT_QUOTES, 'UTF-8') ?>
+    </div>
 </div>
 
 <?php
