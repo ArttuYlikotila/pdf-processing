e845b674a166e95f7a9a2fdea99436602962c25c
Arttu Ylikotila <arttu.ylikotila@tuni.fi>
2026-09-04 15:17:59 +0300

    Fix HTML structure

--- elements/info.php
+++ elements/info.php
@@ -16,32 +16,32 @@
 ?>
 
 <div class="container content-card top-buffer">
-
     <div class="row">
         <div class="col-sm-12">
-<?php if ($processor->returnOk($processingReturnValue)) {
-        // If there is a processed file, offer it to download
-        if (!empty($_SESSION['processedFile']) && file_exists($_SESSION['processedFile'])) {
-            include("elements/download.php");
-        }
-
-    } else { ?>
-
-        <a href="#" class="btn btn-danger btn-lg">
-          <span class="glyphicon glyphicon-remove-sign"></span>
-          <?php echo htmlspecialchars($messages['failMessage'], ENT_QUOTES, 'UTF-8') ?>
-        </a>
-
-<?php } ?>
+        <?php if ($processor->returnOk($processingReturnValue)) {
+            // If there is a processed file, offer it to download
+            if (!empty($_SESSION['processedFile']) && file_exists($_SESSION['processedFile'])) {
+                include("elements/download.php");
+            }
+            } else { ?>
+            <a href="#" class="btn btn-danger btn-lg">
+                <span class="glyphicon glyphicon-remove-sign"></span>
+                <?php echo htmlspecialchars($messages['failMessage'], ENT_QUOTES, 'UTF-8') ?>
+            </a>
+        <?php } ?>
         </div>
     </div>
     <div class="row top-buffer">
-            <div class="col-sm-6">
-                <p><strong><?php echo($messages['deleteMessage']) ?></strong></p>
-            </div>
-            <div class="col-sm-3">
-                <input type="submit" class="btn btn-primary" name="delete_file"
-                        value="<?php echo htmlspecialchars($messages['deleteButton'], ENT_QUOTES, 'UTF-8') ?>">
-            </div>
+        <div class="col-sm-6">
+            <p><strong><?php echo($messages['deleteMessage']) ?></strong></p>
         </div>
+        <div class="col-sm-3">
+            <input
+                type="submit"
+                class="btn btn-primary"
+                name="delete_file"
+                value="<?php echo htmlspecialchars($messages['deleteButton'], ENT_QUOTES, 'UTF-8') ?>"
+            />
+        </div>
+    </div>
 </div>
