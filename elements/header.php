9314a5a45b8e55a19defc0da666438d0cbcd461d
Arttu Ylikotila <arttu.ylikotila@tuni.fi>
2026-09-07 15:13:23 +0300

    Add viewport setting to page head

--- elements/header.php
+++ elements/header.php
@@ -13,6 +13,7 @@
 <html lang="<?php echo $lang ?>">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?php echo htmlspecialchars($messages['htmlTitle'], ENT_QUOTES, 'UTF-8') ?></title>
 
         <base href="<?php echo htmlspecialchars($configs['baseUrl'], ENT_QUOTES, 'UTF-8') ?>">
