4a4ae786753b36f2af81f84b0939bc187a1c019a
Arttu Ylikotila <arttu.ylikotila@tuni.fi>
2026-09-09 16:35:09 +0300

    Remove class from element

--- elements/metadata.php
+++ elements/metadata.php
@@ -14,7 +14,7 @@
  * Input fields for additional metadata.
  */
 ?>
-<div class="row top-buffer">
+<div class="row">
     <div class="info-text mb-3">
         <?php echo $messages['pdfaMetadataMessage'] ?>
     </div>
