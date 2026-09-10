40297cad9bb26be085911619d68d4af2c4370403
Arttu Ylikotila <arttu.ylikotila@tuni.fi>
2026-09-10 14:02:08 +0300

    Make preliminary changes to header

--- elements/header.php
+++ elements/header.php
@@ -30,52 +30,48 @@
         <link rel="icon" type="image/png" sizes="any" href="images/favicon.ico">
     </head>
     <body>
-        <div class="page-header">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-7">
-                        <h3>
-                            <a href="<?php echo htmlspecialchars($messages['logo_link'], ENT_QUOTES, 'UTF-8') ?>" target="_blank">
-                                <img
-                                    src="<?php echo htmlspecialchars($messages['logo_image'], ENT_QUOTES, 'UTF-8') ?>"
-                                    class="logo"
-                                    alt="<?php echo $messages['logo_alt'] ?>"
-                                />
-                            </a>
-                            <?php echo htmlspecialchars($messages['headline'], ENT_QUOTES, 'UTF-8') ?>
-                        </h3>
-                    </div>
-                    <div class="col-sm-3">
-                        <ul class="nav navbar-nav ">
-                            <?php foreach ($messages['navButton'] as $nav) {
-                                $navigator = explode(",", $nav);
-                            ?>
-                                <li class="text-center active">
-                                    <a href="<?php echo htmlspecialchars($navigator[1], ENT_QUOTES, 'UTF-8') ?>">
-                                        <i class="glyphicon <?php echo htmlspecialchars($navigator[2], ENT_QUOTES, 'UTF-8') ?>" aria-hidden="true"></i>
-                                        <?php echo htmlspecialchars($navigator[0], ENT_QUOTES, 'UTF-8') ?>
-                                    </a>
-                                </li>
-                            <?php } ?>
+        <header class="page-header">
+            <nav class="navbar">
+                <div class="container-fluid">
+                    <a class="logo-link" href="<?php echo $messages['logo_link'] ?>" target="_blank">
+                        <img
+                            class="logo"
+                            src="<?php echo $messages['logo_image'] ?>"
+                            alt="<?php echo $messages['logo_alt'] ?>"
+                        />
+                    </a>
+                    <h3><?php echo $messages['headline'] ?></h3>
 
-                            <?php $uri_parts = explode('?', $_SERVER['REQUEST_URI'], 2); ?>
-                            <li class="text-center active"></li>
-                        </ul>
+                    <div class="nav-links">
+                        <?php foreach ($messages['navButton'] as $nav) {
+                            $navigator = explode(",", $nav);
+                        ?>
+                            <a class="nav-link" href="<?php echo $navigator[1] ?>">
+                                <i class="bi <?php echo $navigator[2] ?>" aria-hidden="true"></i>
+                                <?php echo $navigator[0] ?>
+                            </a>
+                        <?php } ?>
+                        <!-- TODO: is this element needed for some reason? -->
+                        <!-- <li class="text-center active"></li> -->
                     </div>
-                    <div class="col-sm-2 lang-selector">
-                        <a href="<?php echo htmlspecialchars($uri_parts[0], ENT_QUOTES, 'UTF-8') ?>?lang=en">
-                            <?php if ($lang === 'en') echo '<strong>' ?>
+
+                    <div class="lang-selector">
+                        <?php $uri_parts = explode('?', $_SERVER['REQUEST_URI'], 2); ?>
+                        <a
+                            href="<?php echo htmlspecialchars($uri_parts[0], ENT_QUOTES, 'UTF-8') ?>?lang=en"
+                            <?php if ($lang === 'en') echo "class='chosen'" ?>
+                        >
                             Englanti
-                            <?php if ($lang === 'en') echo '</strong>' ?>
                         </a>
                         <span> | </span>
-                        <a href="<?php echo htmlspecialchars($uri_parts[0], ENT_QUOTES, 'UTF-8') ?>?lang=fi">
-                            <?php if ($lang === 'fi') echo '<strong>' ?>
+                        <a
+                            href="<?php echo htmlspecialchars($uri_parts[0], ENT_QUOTES, 'UTF-8') ?>?lang=fi"
+                            <?php if ($lang === 'fi') echo "class='chosen'" ?>
+                        >
                             Suomi
-                            <?php if ($lang === 'fi') echo '</strong>' ?>
                         </a>
                     </div>
                 </div>
-            </div>
-        </div>
+            </nav>
+        </header>
         <main class="top-buffer bottom-buffer">
